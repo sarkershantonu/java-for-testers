a7360953664e207e6912d3da3296f84e1fbbb37f
shantonu sarker <shantonu_oxford@yahoo.com>
2019-01-13 18:30:02 -0500

    upto 13th jan

--- collection/collection-examples/src/main/java/org/automation/collection/StringExamples.java
+++ collection/collection-examples/src/main/java/org/automation/collection/StringExamples.java
@@ -1,5 +1,8 @@
 package org.automation.collection;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class StringExamples {
@@ -24,7 +27,25 @@ public class StringExamples {
        /* System.out.println("Without Case >>"+isEqual("JavA", "java"));
         System.out.println("WithCase>>"+isEqualWIthCase("JavA", "java"));
       */
-       System.out.println('a'+ " is in " + "JavaTraining".lastIndexOf(97)+"th place");
+      // System.out.println('a'+ " is in " + "JavaTraining".lastIndexOf(97)+"th place");
+      /*  System.out.println("PRINT="+
+                " StartWithSpace".trim());*/
+    /*  String myEx =String.join("-","Shantonu","Daily","Training");
+       *//* myEx = "JoiningExample".join(
+                "-","Shantonu","Daily","Training"
+        );*//*
+        List<String> items = new ArrayList<>();
+        items = Arrays.asList(new String[]{"Shantonu", "Daily","Training"});
+        String myEx2="Mamun".join("_",items);*/
+        String[] items= "abc1de2f3gh".split("\\d",0);
+
+        String[] items2= "abc1de2f3gh".split("[a-z]+");
+
+        for(String item:items2){
+            System.out.println(item);
+        }
+
+
     }
 
     public static void stringOperation() {
