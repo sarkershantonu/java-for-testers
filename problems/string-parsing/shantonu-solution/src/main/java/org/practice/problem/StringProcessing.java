5dffd096b39bef5f83b2a675fda3b4df884b203b
shantonu sarker <shantonu_oxford@yahoo.com>
2019-01-23 12:59:29 -0500

    updated upto 20th

--- problems/string-parsing/shantonu-solution/src/main/java/org/practice/problem/StringProcessing.java
+++ problems/string-parsing/shantonu-solution/src/main/java/org/practice/problem/StringProcessing.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
+package org.practice.problem;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringProcessing {
+    private String para;
+    private final String aux="am|is|are|was|were|be|can|being|been|do|does|did|has|have|had|having|Should|might";
+    private final String verbs=aux+"|called|appear|derived|deleting|changing|need|detailed|calls|we’ll|making|able|make|preserves";
+
+    public int getWordCount() {
+        return getMatch("\\w+");
+    }
+
+    public void setParagraph(String word) {
+        this.para = word;
+    }
+
+    public int getVerbCount() {
+        return getMatch("^"+verbs+"\\s");
+    }
+
+    public int getAuxVerbCount() {
+        return getMatch("^"+aux+"\\s");
+    }
+
+    public int getTheCount() {
+        return getMatch("\\sthe\\s");
+    }
+
+    private int getMatch(String pattern){
+        Matcher matcher = Pattern.compile(pattern).matcher(para);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Pattern p = Pattern.compile("\\w+");
+        Matcher m = p.matcher("This is String for parsing where 12345 is number and others are String");
+
+
+    }
 }
--- problems/string-parsing/shantonu-solution/src/test/java/org/practice/problem/TestSolution.java
+++ problems/string-parsing/shantonu-solution/src/test/java/org/practice/problem/TestSolution.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+package org.practice.problem;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class TestSolution {
+    private StringProcessing processor;
+    private String word = "A pattern P is called a subsequence of Text T if the pattern preserves the relative ordering of characters within the text T and it might not appear in a continuous fashion";
+    @Before
+    public void init(){
+        processor = new StringProcessing();
+    }
+    @Test
+    public void testWordCount(){
+        processor.setParagraph(word);//mamun
+        Assert.assertEquals("Word Count", 32,processor.getWordCount());
+    }
+    @Test
+    public void testVerbCount(){
+        processor.setParagraph(word);//tasnia
+        Assert.assertEquals("Verb Count",5,processor.getVerbCount());
+    }
+    @Test
+    public void testAuxVerbCount(){
+        processor.setParagraph(word);//tushar
+        Assert.assertEquals("Verb Count",2,processor.getAuxVerbCount());
+    }
+    @Test
+    public void testTheCount(){
+        processor.setParagraph(word);//sajib
+        Assert.assertEquals("Verb Count",3,processor.getTheCount());
+    }
 }
