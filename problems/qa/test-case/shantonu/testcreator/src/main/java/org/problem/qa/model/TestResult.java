28f9945352e8f1b602cd323cdb65b9bb0de5c8f9
shantonu sarker <shantonu_oxford@yahoo.com>
2019-03-11 23:49:10 -0400

    result model

--- problems/qa/test-case/shantonu/testcreator/src/main/java/org/problem/qa/model/TestResult.java
+++ problems/qa/test-case/shantonu/testcreator/src/main/java/org/problem/qa/model/TestResult.java
@@ -1,17 +1,27 @@
-package org.problem.qa;
+package org.problem.qa.model;
 
 import java.time.LocalDate;
 
 public class TestResult {
-    private Long id;
+    private String id;
     private String requestType;
     private String response;
     private Boolean success;
 
-    public TestResult(Long id, String requestType, String response, Boolean success) {
+    public TestResult(String id, String requestType, String response, Boolean success) {
         this.id = id;
         this.requestType = requestType;
         this.response = response;
         this.success = success;
+    }
+
+    @Override
+    public String toString() {
+        return "TestResult{" +
+                "id=" + id +
+                ", requestType='" + requestType + '\'' +
+                ", response='" + response + '\'' +
+                ", success=" + success +
+                '}';
     }
 }
